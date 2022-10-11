afff2ae200a4b5bbe4abc9f8877f6f35b666dd0a
Dongho Seo <ehcws333@gmail.com>
2022-10-11 17:49:53 +0900

    :lipstick: overall progress 다시 apexchart 로 돌아가기

--- src/Component/ProgressBar/CircularProgressBar.tsx
+++ src/Component/ProgressBar/CircularProgressBar.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import styled from "styled-components";
-import { Doughnut } from "react-chartjs-2";
+import ReactApexChart from "react-apexcharts";
 import useGradStatusValue from "../../Hooks/Grad/useGradStatusValue";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
+import { ApexOptions } from "apexcharts";
 
 const CircularProgressContainer = styled.div`
   box-sizing: border-box;
   width: 380px;
   height: 380px;
-  padding: 40px;
   border-radius: 16px;
   background-color: #fff;
   display: flex;
@@ -23,35 +20,65 @@ const CircularProgressContainer = styled.div`
 function CircularProgressBar() {
   const valueContext = useGradStatusValue();
 
-  let creditsLeft = 130;
   let creditsEarned = 0;
-
   if (typeof valueContext !== undefined) {
     creditsEarned = valueContext?.totalCredits as number;
-    creditsLeft = 130 - creditsEarned;
   }
 
-  const gradOverallData = {
-    datasets: [
-      {
-        // label: "# of Votes",
-        data: [creditsEarned, creditsLeft],
-        backgroundColor: ["rgb(25, 31, 40)", "#f2f4f6"],
-        // borderColor: ["#abf2d4", "#f2f4f6"],
-        borderWidth: 1,
-      },
-    ],
+  const calcPercent = (current: number, total: number) => {
+    return current <= total ? Math.round((current * 100) / total) : 100;
   };
 
-  const options = {
-    maintainAspectRatio: false,
-    cutout: 120,
+  const options: ApexOptions = {
+    chart: {
+      type: "radialBar",
+    },
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          size: "80%",
+        },
+        dataLabels: {
+          name: {
+            // 여기는 퍼센트가 표시되는 라벨 이름
+            offsetY: -10,
+            show: false,
+            color: "#333d4b",
+            fontSize: "24px",
+            fontWeight: "medium",
+          },
+          value: {
+            // 여기는 퍼센트 숫자 조절
+            color: "#111",
+            fontSize: "20px",
+            show: false,
+            formatter: (val: number) => {
+              return val + " 학점";
+            },
+          },
+        },
+        track: {
+          background: "#fff",
+        },
+      },
+    },
+    stroke: {
+      lineCap: "round",
+    },
   };
+
+  const series = [calcPercent(creditsEarned, 130)];
 
   return (
     <CircularProgressContainer>
       {valueContext ? (
-        <Doughnut data={gradOverallData} options={options} />
+        // <Doughnut data={gradOverallData} options={options} />
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="radialBar"
+          height={360}
+        />
       ) : (
         <p>서버에서 값 받아오는중!</p>
       )}
